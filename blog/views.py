bdeb7dceb0001224bc39363bc09ce10b2a1cb19c
dishit2141 <dishit2141@gmail.com>
2021-04-14 18:56:31 +0530

    replies are shown

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from blog.models import Post,BlogComment
 from django.contrib import messages
 import datetime
 from .forms import *
+from blog.templatetags import extras
 
 # Create your views here.
 
@@ -15,8 +16,16 @@ def bloghome(request):
 
 def blogpost(request,slug):
     post=Post.objects.filter(slug=slug).first()
-    comments = BlogComment.objects.filter(post=post)
-    context={'post':post,'comments':comments,'user':request.user}
+    comments = BlogComment.objects.filter(post=post,parent=None)
+    replies = BlogComment.objects.filter(post=post).exclude(parent=None)
+    replyDict = {}
+    for reply in replies:
+        if reply.parent.Sno not in replyDict.keys():
+            replyDict[reply.parent.Sno]=[reply]
+        else:
+            replyDict[reply.parent.Sno].append(reply)
+            
+    context={'post':post,'comments':comments,'user':request.user,'replyDict':replyDict}
 
     return render(request,"blogtemp/blogpost.html",context)
 
@@ -42,9 +51,16 @@ def postComment(request):
         user = request.user
         postSno =  request.POST.get("postSno")
         post = Post.objects.get(Sno=postSno)
-        comment = BlogComment(comment=comment,user=user,post=post)
-        comment.save()
-        messages.success(request,"Your comment has been posted successfully!")
+        ParentSno =request.POST.get("ParentSno")
+        if ParentSno == "":
+            comment = BlogComment(comment=comment,user=user,post=post)
+            comment.save()
+            messages.success(request,"Your comment has been posted successfully!")
+        else:
+            parent=BlogComment.objects.get(Sno=ParentSno)
+            comment = BlogComment(comment=comment,user=user,post=post,parent=parent)
+            comment.save()
+            messages.success(request,"Your reply has been posted successfully!")
 
 
 
